f50a1ab1e23e53987a1b4454d32b294e50e4fc0a
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-04-08 12:33:03 +0700

    Authentication with NextAuth

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="font-work-sans">
+        <Navbar />
+        
+        {children}
+    </main>
+  );
+}
