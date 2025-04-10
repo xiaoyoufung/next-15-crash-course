3bad401d7989ab6a17c46bea65a8019535a0ee00
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-04-10 22:56:03 +0700

    Home Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container pattern">
+        <h1 className="heading">Pitch your startup, <br />
+          Connect with Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitched, and Get noticed by Investors.
+        </p>
+      </section>
     </>
   );
 }
