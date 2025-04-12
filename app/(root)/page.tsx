4a93b1a4eefebd023d3d262584ac5fca17b6a5b7
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-04-12 12:12:03 +0700

    All Startups & Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
+// import { auth } from "@/auth";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+type StartupCardType = {
+  _createdAt: string;
+  views: number;
+  author: { _id: number, name: string };
+  _id: number;
+  description: string;
+  image: string;
+  category: string;
+  title: string;
+};
 
 export default async function Home({searchParams}: { searchParams: Promise<{query?:string}> }) {
   const query = (await searchParams).query;
 
+  const post = [ {
+    _createdAt: new Date().toISOString(),
+    views: 100,
+    author: {_id: 1, name: "John Doe"},
+    _id: 1,
+    description: "This is a description",
+    image: "https://i.ytimg.com/vi/6v6dbxPlsXs/maxresdefault.jpg",
+    category: "Robots",
+    title: "We Robots",
+  }]
 
   return (
     <>
@@ -16,6 +39,26 @@ export default async function Home({searchParams}: { searchParams: Promise<{quer
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-[30px] font-semibold">
+          {query ? `Search results for "${query}"` : "Latest Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {post?.length > 0 ? (
+            post.map((post: StartupCardType) => (
+              <StartupCard
+                key={post._id}
+                post={post}
+              />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,72 @@
+import { EyeIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+type StartupCardType = {
+    _createdAt: string;
+    views: number;
+    author: { _id: number, name: string };
+    _id: number;
+    description: string;
+    image: string;
+    category: string;
+    title: string;
+};
+
+const StartupCard = ({ post }: { post: StartupCardType }) => {
+    const { _createdAt, views, author: { _id: authorId, name }, _id, description, image, category, title } = post;
+
+    return (
+        <li className='startup-card group'>
+            <div className='flex-between'>
+                <p className='startup_card_date'>
+                    {_createdAt}
+                </p>
+
+                <div className='flex gap-1.5'>
+                    <EyeIcon className='size-6 text-primary-default' />
+                    <span className="text-[16px] font-medium">{views}</span>
+                </div>
+            </div>
+
+            <div className="flex-between mt-5 gap-5">
+                <div>
+                    <Link href={`/user/${authorId}`}>
+                        <p>{name}</p>
+                    </Link>
+
+                    <Link href={`/startup/${_id}`}>
+                        <h3 className='text-[26px] font-semibold'>
+                            {title}
+                        </h3>
+                    </Link>
+                </div>
+
+                <Link href={`/user/${authorId}`}>
+                    <img src="https://placehold.co/48x48" alt="placeholder" className='w-[48px] h-[48px] object-cover rounded-full' />
+                </Link>
+            </div>
+
+            <Link href={`/startup/${_id}`}>
+                <p className="startup-card_desc">
+                    {description}
+                </p>
+
+                <img src={image} alt="placeholder" className="startup-card_img" />
+            </Link>
+
+            <div className="flex-between gap-3 mt-5">
+                <Link href={`/query=${category.toLowerCase()}`}>
+                    <p>{category}</p>
+                </Link>
+                <Button className="startup-card_btn" asChild>
+                    <Link href={`/startup/${_id}`}>
+                        <span>Detailts</span>
+                    </Link>
+                </Button>
+            </div>
+        </li>
+    )
+}
+
+export default StartupCard
