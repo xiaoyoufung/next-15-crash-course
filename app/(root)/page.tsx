68a2b44d4217a918e449cd56eba2aaf1ebc86108
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-04-18 02:12:03 +0700

    Sanity Write Client

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 // import { auth } from "@/auth";
 import SearchForm from "../../components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
-import { STARTUP_QUERY } from "@/lib/queries";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({searchParams}: { searchParams: Promise<{query?:string}> }) {
--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/lib/queries';
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
 import { formatDate } from '@/lib/utils';
 import Link from 'next/link';
